826febbb1c3e9de9e843330b15dbf4c196ca9149
ellamarym <ellamuir@icloud.com>
2022-12-21 16:34:55 +0000

    rejigs listen

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,15 @@
 const mongoose = require("mongoose");
 require("dotenv/config");
+const app = require("../app")
 mongoose.set("strictQuery", true);
-mongoose.connect(process.env.DATABASE_URL, () => {
-  console.log("connection is now made here!");
-});
+mongoose.connect(process.env.DATABASE_URL) 
+ mongoose.connection.once("open", ()=> {
+  console.log("connection is now made here!"),
+  app.listen(8000, ()=> {
+    console.log('listening port 8000')
+  });
+ }
+);
 mongoose.connection.on("error", (err) => {
   console.log(err);
 });
